24ab4910feed7fb1f8ea874cc8aeb2189a9bbb58
24520881-LeAnhKien <24520881@gm.uit.edu.vn>
2025-11-15 18:38:10 +0700

    Update VeKhung.py

--- VeKhung.py
+++ VeKhung.py
@@ -1,7 +1,18 @@
 import pygame
 
-# ==========================
-# Hàm vẽ khung
-# ==========================
+
+#define MINX 2
+#define MINY 2
+#define MAXX 35
+#define MAXY 20
+void VeKhung(){
+    for (int i = MINX ; i<=MAXX ; i++)
+        for (int j = MINY ; j<=MAXY ; j++)
+            if ((i==MINX) || (i==MAXX) || (j==MINY) || (j==MAXY)){
+                gotoxy(i,j);
+                printf("+");
+            }
+}
+
 def VeKhung(screen, width, height):
     pygame.draw.rect(screen, (255, 255, 255), (0, 0, width, height), 10)
